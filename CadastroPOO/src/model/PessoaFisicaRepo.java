2166f0eab5be5fe4e57d0358fe8461bc89352f05
MariLFO <mari.fer.onorio@gmail.com>
2023-05-22 16:45:48 -0700

    Métodos inserir, alterar e excluir adicionados em PessoaFisicaRepo

---
+++ CadastroPOO/src/model/PessoaFisicaRepo.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PessoaFisicaRepo {
+    private ArrayList<PessoaFisica>  pessoasFisicas;
+
+    public void inserir(PessoaFisica entidade) {
+
+    }
+
+    public void alterar(PessoaFisica entidade){
+
+    }
+
+    public void  excluir(int id){
+
+    }
+
+    public ArrayList<PessoaFisica> obterTodos(){
+        return this.pessoasFisicas;
+
+    }
+}
