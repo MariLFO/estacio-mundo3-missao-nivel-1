e255cafa89e34b877c45ff043b6e30d8e314942c
MariLFO <mari.fer.onorio@gmail.com>
2023-05-21 19:56:12 -0700

    Classe Pessoa criada

--- CadastroPOO/src/Main.java
+++ CadastroPOO/src/Main.java
@@ -1,5 +1,19 @@
+import model.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(1, "João");
+        p1.exibir();
+        Pessoa p2 = new Pessoa(2, "Maria");
+        p2.exibir();
 
+        p1.getId();
+        p1.exibir();
+
+        p1.getId();
+        p1.exibir();
+
+        p2.exibir();
     }
+
 }
---
+++ CadastroPOO/src/model/Pessoa.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+
+    public Pessoa(){
+
+    }
+    public Pessoa(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+    public void exibir() {
+        System.out.println("id: "+ this.id + ", nome: "+ this.nome);
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
