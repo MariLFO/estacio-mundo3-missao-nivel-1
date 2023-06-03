e9a8859497c657a7d52d436db0cdece297fbe122
MariLFO <mari.fer.onorio@gmail.com>
2023-06-03 16:44:14 -0700

    PessoaJuridicaRepo atualizada

--- CadastroPOO/src/model/PessoaFisicaRepo.java
+++ CadastroPOO/src/model/PessoaFisicaRepo.java
@@ -6,15 +6,16 @@ public class PessoaFisicaRepo {
     private ArrayList<PessoaFisica>  pessoasFisicas;
 
     public void inserir(PessoaFisica entidade) {
-
+        this.pessoasFisicas.add(entidade);
     }
 
     public void alterar(PessoaFisica entidade){
-
+        this.excluir(entidade.id);
+        this.inserir(entidade);
     }
 
     public void  excluir(int id){
-
+        this.pessoasFisicas.removeIf(item -> item.id == id);
     }
 
     public ArrayList<PessoaFisica> obterTodos(){
--- CadastroPOO/src/model/PessoaJuridicaRepo.java
+++ CadastroPOO/src/model/PessoaJuridicaRepo.java
@@ -6,14 +6,16 @@ public class PessoaJuridicaRepo {
     private ArrayList<PessoaJuridica> pessoasJuridicas;
 
     public void inserir(PessoaJuridica entidade) {
-
+        this.pessoasJuridicas.add(entidade);
     }
 
     public void alterar(PessoaJuridica entidade){
+        this.excluir(entidade.id);
+        this.inserir(entidade);
     }
 
     public void excluir(int id){
-
+        this.pessoasJuridicas.removeIf(item -> item.id == id);
     }
 
     public ArrayList<PessoaJuridica> obterTodos(){
